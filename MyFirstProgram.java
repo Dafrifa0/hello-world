8448bc069c1e719cce0a19d218436a8f54037d9a
Dafrifa0 <dafrifa0@email.cpcc.edu>
2023-02-26 18:51:31 -0500

    Revert "Modification"
    This reverts commit 5da5f774095dfed25e3918e87a3d1b9a615caae2.

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -4,7 +4,5 @@ public class MyFirstProgram
    public static void main(String[] args)
    {
    System.out.println("Hello World!");
-   
-   System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
    }
 }
